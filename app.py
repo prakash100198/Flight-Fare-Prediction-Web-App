a45bd7e457b1d69dfc25698c4f574022d1ee4a0c
Prakash Kumar <54064843+prakash100198@users.noreply.github.com>
2020-12-21 15:52:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,11 +32,11 @@ def predict():
         c= pd.Series(airline_value)
         airline = c.map(airline_dict).values[0]   #<----------
 
-        day = int(date_value.split(' ')[0].split('-')[0])    #<----------------
-        month = int(date_value.split(' ')[0].split('-')[1])  #<---------
+        day = int(pd.to_datetime(date_value, format="%Y-%m-%dT%H:%M").day)    #<----------------
+        month = int(pd.to_datetime(date_value, format="%Y-%m-%dT%H:%M").month)  #<---------
 
-        hour = int(date_value.split(' ')[1].split(':')[0])
-        minute = int(date_value.split(' ')[1].split(':')[1])
+        hour = int(pd.to_datetime(date_value, format ="%Y-%m-%dT%H:%M").hour)
+        minute = int(pd.to_datetime(date_value, format ="%Y-%m-%dT%H:%M").minute)
 
         pred_features = [np.array([day,month,stops_value,hour,minute,airline,source,destination])]
         prediction = model.predict(pred_features)
